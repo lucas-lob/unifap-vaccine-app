f72d4e1a7195c11e55c86f4aa2c1cd8a9028480c
Lucas Lobato <lucas.mcp23@gmail.com>
2026-06-22 20:43:13 -0300

    feat: add component to place card

---
+++ components/molecules/PlaceCard/styles.ts
@@ -0,0 +1,43 @@
+import {
+  COLOR,
+  FONT_SIZE,
+  FONT_WEIGHT,
+  LINE_HEIGHT,
+  SPACING
+} from "@/style/tokens";
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: SPACING.SM,
+    padding: SPACING.MD,
+    backgroundColor: COLOR.WHITE
+  },
+  title: {
+    marginBottom: SPACING.SM,
+    color: COLOR.GRAY_900,
+    fontSize: FONT_SIZE.BASE,
+    lineHeight: LINE_HEIGHT.BASE,
+    fontWeight: FONT_WEIGHT.MEDIUM
+  },
+  textContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: SPACING.SM
+  },
+  text: {
+    color: COLOR.GRAY_600,
+    fontSize: FONT_SIZE.BASE,
+    lineHeight: LINE_HEIGHT.BASE,
+    fontWeight: FONT_WEIGHT.REGULAR
+  },
+  buttonsContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    gap: SPACING.SM,
+    marginTop: SPACING.SM
+  }
+})
